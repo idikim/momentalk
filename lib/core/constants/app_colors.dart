01a5a1f44ab610ecd839c747e565a72d0d89b8d6
hyeji_kim <hjkim9217@naver.com>
2025-03-13 23:17:50 +0900

    add: Color 상수 파일 추가

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color grayScale_150 = Color(0xFFFAFAFA);
+  static const Color grayScale_250 = Color(0xFFEBEBEB);
+  static const Color grayScale_350 = Color(0xFFBDBDBD);
+  static const Color grayScale_450 = Color(0xFF848484);
+  static const Color grayScale_550 = Color(0xFF666666);
+  //
+  static const Color red = Color(0xFFFFD1D1);
+  static const Color blue = Color(0xFFD3EEFF);
+  static const Color green = Color(0xFFD9FFA5);
+  static const Color yellow = Color(0xFFFFF7B1);
+}
